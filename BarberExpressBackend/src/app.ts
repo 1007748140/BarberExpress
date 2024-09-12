6ba8229a895b6acd0856660068521bd0aad91da9
Yadir <yadamuoz@misena.edu.co>
2024-09-12 12:58:05 -0500

    Estructura basica-inicial

---
+++ BarberExpressBackend/src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Servidor de BarberExpress funcionando');
+});
+
+mongoose.connect('mongodb://localhost:27017/barberexpress')
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(error => console.error('Error al conectar a MongoDB:', error));
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
