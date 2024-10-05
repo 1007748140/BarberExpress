169228315086906a454c5754a63ea5966ff03162
Yadir <yadamuoz@misena.edu.co>
2024-10-04 20:54:50 -0500

    Correcion de inicializacion de proyecto app

--- BarberExpressBackend/src/app.ts
+++ BarberExpressBackend/src/app.ts
@@ -1,20 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import pool from './config/db';
+import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-dotenv.config();
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// Middleware y rutas
-app.get('/', async (_req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM user');
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching data from database');
-  }
+app.get('/', (req: Request, res: Response) => {
+  res.send('Servidor de BarberExpress funcionando');
 });
 
-export default app;
+mongoose.connect('mongodb://localhost:27017/barberexpress')
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(error => console.error('Error al conectar a MongoDB:', error));
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
